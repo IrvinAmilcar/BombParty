782458ca576e2da792f4aeabbc3b1160a39104dc
Henrique Figueiredo <hft@cesar.school>
2025-05-01 12:54:10 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -6,37 +6,42 @@ typedef enum GameState {
     GAME_OVER
 } GameState;
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
     const int screenWidth = 800;
     const int screenHeight = 450;
 
     InitWindow(screenWidth, screenHeight, "BombParty");
 
+    Font customFont = LoadFontEx("resources/fonts/Montserrat-Regular.ttf", 40, NULL, 0);
+    Font textFont;
+
+    if (customFont.texture.id == 0)
+    {
+        TraceLog(LOG_ERROR, "Failed to load font: resources/fonts/Montserrat-Regular.ttf");
+        textFont = GetFontDefault();
+    }
+    else
+    {
+        TraceLog(LOG_INFO, "Successfully loaded font: resources/fonts/Montserrat-Regular.ttf");
+        textFont = customFont;
+    }
+
     Imgage bomb = LoadImage("../resources/textures/bomb.png");
     Imgage spark = LoadImage("../resources/textures/spark.png");
     Imgage arrow = LoadImage("../resources/textures/arrow.png");
 
-    SetTargetFPS(60); // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-
     GameState currentGameState = MENU;
-
-    // Main game loop
-    while (!WindowShouldClose()) // Detect window close button or ESC key
+    
+    SetTargetFPS(60);
+    
+    while (!WindowShouldClose()) 
     {
-        // Update
-        //----------------------------------------------------------------------------------
         switch (currentGameState)
         {
             case MENU:
             {
-                if (IsKeyPressed(KEY_ENTER))
+                if (IsKeyPressed(KEY_1))
                 {
                     currentGameState = PLAYING; 
                     // TODO: Inicializar variaveis do jogo (timer, jogadores, etc.)
@@ -52,8 +57,8 @@ int main(void)
                 // - Checar regras do jogo (palavra valida, passar a bomba)
                 // - Verificar condicoes de fim de jogo (timer esgotou, jogador eliminado)
 
-                // Exemplo simples de transicao para GAME_OVER (pressione G no modo PLAYING):
-                if (IsKeyPressed(KEY_G))
+                // Exemplo simples de transicao para GAME_OVER (pressione 2 no modo PLAYING):
+                if (IsKeyPressed(KEY_2))
                 {
                     currentGameState = GAME_OVER;
                      // TODO: Salvar pontuacao final ou preparar tela de Game Over
@@ -66,8 +71,8 @@ int main(void)
             case GAME_OVER:
             {
                 // Logica de Update para o Game Over:
-                // - Esperar por input para reiniciar o jogo (ex: R)
-                if (IsKeyPressed(KEY_R))
+                // - Esperar por input para reiniciar o jogo (ex: 3)
+                if (IsKeyPressed(KEY_3))
                 {
                     currentGameState = MENU; // Muda de volta para o estado de MENU
                     // TODO: Reinicializar todas as variaveis do jogo para comecar de novo
@@ -77,7 +82,6 @@ int main(void)
 
             default: break;
         }
-        //----------------------------------------------------------------------------------
 
         // Draw
         //----------------------------------------------------------------------------------
@@ -89,33 +93,28 @@ int main(void)
             {
                 case MENU:
                 {
-                    // Desenhar a tela de Menu: Titulo, instrucoes, etc.
                     DrawText("Bomb Party", screenWidth/2 - MeasureText("Bomb Party", 40)/2, screenHeight/3, 40, GRAY);
-                    DrawText("Pressione ENTER para Comecar", screenWidth/2 - MeasureText("Pressione ENTER para Comecar", 20)/2, screenHeight/2, 20, DARKGRAY);
+                    DrawText("Pressione 1 para Comecar", screenWidth/2 - MeasureText("Pressione 1 para Comecar", 20)/2, screenHeight/2, 20, DARKGRAY);
 
                 } break;
 
                 case PLAYING:
                 {
-                    // Desenhar a tela de Jogo: Timer, silaba, caixa de input, bomba, etc.
                     DrawText("Estado: JOGANDO", 10, 10, 20, BLACK);
                     // TODO: Desenhar elementos do jogo (timer, silaba, input, bomba, jogadores)
 
-                } break; // Fim do case PLAYING (Draw)
+                } break;
 
                 case GAME_OVER:
                 {
-                    // Desenhar a tela de Game Over: Mensagem, pontuacao, instrucoes para reiniciar.
                      DrawText("Fim de Jogo!", screenWidth/2 - MeasureText("Fim de Jogo!", 40)/2, screenHeight/3, 40, DARKGRAY);
-                     DrawText("Pressione R para Reiniciar", screenWidth/2 - MeasureText("Pressione R para Reiniciar", 20)/2, screenHeight/2, 20, GRAY);
+                     DrawText("Pressione 3 para Reiniciar", screenWidth/2 - MeasureText("Pressione 3 para Reiniciar", 20)/2, screenHeight/2, 20, GRAY);
                     // TODO: Desenhar pontuacao final
+                } break; 
 
-                } break; // Fim do case GAME_OVER (Draw)
-
-                default: break; // Estado padrao, nao deve acontecer
+                default: break;
             }
 
-            // Elementos que sao desenhados SEMPRE, independentemente do estado (ex: FPS)
             DrawFPS(10, screenHeight - 20);
 
         EndDrawing();
@@ -124,7 +123,17 @@ int main(void)
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
-     // TODO: Descarregar quaisquer assets (texturas, sons, fontes) que foram carregados
+    // TODO: Descarregar quaisquer assets (texturas, sons, fontes) que foram carregados
+    if (customFont.texture.id != 0)
+    {
+        UnloadFont(customFont); 
+    }
+    
+    // Note: bombImage was already unloaded after creating the texture.
+    UnloadTexture(bombTexture);
+    UnloadTexture()
+
+
     CloseWindow();
     //--------------------------------------------------------------------------------------
 
