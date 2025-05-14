f20a00223fdd38832dc074fe7308ff5f1b15c0a3
Henrique Figueiredo <hft@cesar.school>
2025-05-14 02:03:24 -0300

    Revert "modularizar game é pedir pra dar erro..."
    This reverts commit 433c7989ed12f3974f67d38404b2bf65b0091b32.

---
+++ src/game.h
@@ -0,0 +1,15 @@
+#ifndef GAME_H
+#define GAME_H
+
+typedef enum GameState {
+    MENU = 0,
+    PLAYING,
+    GAME_OVER
+} GameState;
+
+#define MAX_WORD_LENGTH 64
+#define MAX_SYLLABLE_LENGTH 3
+#define MIN_SYLLABLE_LENGTH 2
+#define MAX_PLAYER_INPUT_CHARS 30
+
+#endif
